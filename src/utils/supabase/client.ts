b6f0bf8bf6d12e2050cf86e182a10c2eea0fbdc3
NOVALITELTD <166810724+NOVALITELTD@users.noreply.github.com>
2026-03-06 18:00:34 +0100

    Update client.ts

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -5,9 +5,3 @@ export const createClient = () =>
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!
   )
-```
-
-Also make sure these two are in your Vercel environment variables:
-```
-NEXT_PUBLIC_SUPABASE_URL=https://kyzsfcstbpkpezwwnicj.supabase.co
-NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY=sb_publishable_gcK8w_7LVV2vMWWm8t0uiA_ehbcjAN4
